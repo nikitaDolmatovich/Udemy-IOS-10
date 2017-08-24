91f8625f4b3d0d9c7051ce79ecdffc9505581e64
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-08-24 12:35:04 +0300

    Added initial cell commit

--- To Do List/To Do List/FirstViewController.swift
+++ To Do List/To Do List/FirstViewController.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return 0
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
