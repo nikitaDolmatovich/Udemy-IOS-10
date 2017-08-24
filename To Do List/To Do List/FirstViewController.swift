157f94d4a066ad9493339df4767a06b93021928f
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-08-24 10:06:58 +0300

    Project "To Do List" initial commit

---
+++ To Do List/To Do List/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  To Do List
+//
+//  Created by Nikita on 24.08.17.
+//  Copyright © 2017 SolIT. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
